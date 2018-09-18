95a75004e209e7091cb5f2a99c12984c0f610a2a
Fennet <jasper.fennet@capgemini.com>
2018-09-18 11:59:22 +0200

    fight cycle and main implementation

--- src/com/capgemini/HungerGames.java
+++ src/com/capgemini/HungerGames.java
@@ -1,13 +1,13 @@
 package com.capgemini;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HungerGames {
     public static final int AMOUNTCONTESTANTS = 24;
     public static final int FIGHTCYCLES = 2;
-
+    private static Random random = new Random();
     private ArrayList<Contestant> contestants;
-
 
     public HungerGames() {
         this.contestants = new ArrayList<Contestant>();
@@ -16,5 +16,42 @@ public class HungerGames {
 
     public ArrayList<Contestant> getContestants() {
         return contestants;
+    }
+
+
+
+    public void fightCycle() {
+        int damage;
+
+        if (getContestants().size() == 1) {
+            System.out.printf("Winner Hungergames is Contestant %d\n", getContestants().get(0).getId());
+        } else {
+
+            Contestant contestantOne = getContestants().get(random.nextInt(getContestants().size()));
+            Contestant contestantTwo = getContestants().get(random.nextInt(getContestants().size()));
+
+            System.out.printf("Look!! A fight is starting\n");
+            damage = contestantOne.attack();
+            System.out.printf("Contestant %d Attacks Contestant %d with a damage of %d\n", contestantOne.getId(), contestantTwo.getId(), damage);
+            contestantTwo.takeDamage(damage);
+
+            if (contestantTwo.isDead()) {
+                contestants.remove(contestantTwo);
+                System.out.printf("Contestant %d killed Contestant %d, %d Contestants left.\n", contestantOne.getId(), contestantTwo.getId(), contestants.size());
+            } else {
+                System.out.printf("Contestant %d now has %d health left.\n", contestantTwo.getId(), contestantTwo.getHealth());
+
+                damage = contestantTwo.attack();
+                System.out.printf("Contestant %d Attacks Contestant %d with a damage of %d\n", contestantTwo.getId(), contestantOne.getId(), damage);
+                contestantOne.takeDamage(damage);
+            }
+
+            if (contestantOne.isDead()) {
+                contestants.remove(contestantOne);
+                System.out.printf("Contestant %d killed Contestant %d, %d Contestants left.\n", contestantTwo.getId(), contestantOne.getId(), contestants.size());
+            } else {
+                System.out.printf("Contestant %d now has %d health left.\n", contestantTwo.getId(), contestantTwo.getHealth());
+            }
+        }
     }
 }
--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -4,16 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         HungerGames hungerGames = new HungerGames();
+//        for (Contestant contestant : hungerGames.getContestants()){
+//            System.out.println(contestant.toString());
+//        }
         printIntroHungergames(hungerGames);
-
-
+        while (hungerGames.getContestants().size() >= 1) {
+            hungerGames.fightCycle();
+        }
 
     }
 
-    public static void printIntroHungergames(HungerGames hungerGames){
+    public static void printIntroHungergames(HungerGames hungerGames) {
         System.out.printf("*** This year's Hunger Games contains %s contestants.\n", hungerGames.getContestants().size());
         System.out.printf("*** During this years hunger games we let contestants fight untill we have a winner.\n");
         System.out.printf("*** There are a total of %d fight cycles each day. \n", HungerGames.FIGHTCYCLES);
-        System.out.printf("*** In a single fight cycle a contestant gets to attack and defence once, unless a user is killed before defence state.\n");
+        System.out.printf("*** In a single fight cycle a contestant gets to attack and heal once, unless a user is killed before heal state.\n");
     }
 }
