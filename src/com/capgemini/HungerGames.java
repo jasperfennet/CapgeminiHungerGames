63b719b9649ee7bf94c0508159d85507e13b1114
Jasper Fennet <jasper.fennet@gmail.com>
2018-09-22 17:17:09 +0200

    Refactor main and move flow to hungergames and contestant

--- src/com/capgemini/HungerGames.java
+++ src/com/capgemini/HungerGames.java
@@ -1,20 +1,42 @@
 package com.capgemini;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HungerGames {
-    public static final int AMOUNTCONTESTANTS = 24;
-    public static final int FIGHTCYCLES = 2;
 
+    public static final int AMOUNTCONTESTANTS = 24;
     private ArrayList<Contestant> contestants;
 
-
     public HungerGames() {
-        this.contestants = new ArrayList<Contestant>();
-        this.contestants = Contestant.generateContestants(contestants);
+        generateContestants();
+        start();
+    }
+
+    private void start() {
+        printIntroHungergames();
+
+
     }
 
     public ArrayList<Contestant> getContestants() {
         return contestants;
+    }
+
+    public void generateContestants() {
+        Random random = new Random();
+
+        this.contestants = new ArrayList<Contestant>();
+
+        while (this.contestants.size() != HungerGames.AMOUNTCONTESTANTS) {
+            this.contestants.add(new Contestant(random.nextInt(150), random.nextInt(15), random.nextInt(15), false));
+            this.contestants.add(new Contestant(random.nextInt(150), random.nextInt(15), random.nextInt(15), true));
+        }
+    }
+
+    public void printIntroHungergames() {
+        System.out.printf("*** This year's Hunger Games contains %s contestants.\n", contestants.size());
+        System.out.printf("*** During this years hunger games we let contestants fight untill we have a winner.\n");
+        System.out.printf("*** In a single fight cycle a contestant gets to attack and defence once, unless a user is killed before defence state.\n");
     }
 }
--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -4,16 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         HungerGames hungerGames = new HungerGames();
-        printIntroHungergames(hungerGames);
-
-
-
-    }
-
-    public static void printIntroHungergames(HungerGames hungerGames){
-        System.out.printf("*** This year's Hunger Games contains %s contestants.\n", hungerGames.getContestants().size());
-        System.out.printf("*** During this years hunger games we let contestants fight untill we have a winner.\n");
-        System.out.printf("*** There are a total of %d fight cycles each day. \n", HungerGames.FIGHTCYCLES);
-        System.out.printf("*** In a single fight cycle a contestant gets to attack and defence once, unless a user is killed before defence state.\n");
     }
 }
